2d75db6b43ff544d7e58607abd035b30b8fba885
Iñigo <Iigo>
2019-02-02 20:38:47 -0500

    problems with lombok on gradle, only works on ide

--- src/main/java/everis/conocimiento/model/Color.java
+++ src/main/java/everis/conocimiento/model/Color.java
@@ -1,18 +1,35 @@
 package everis.conocimiento.model;
+import com.fasterxml.jackson.databind.JsonSerializable;
+import io.micronaut.core.serialize.ObjectSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.io.Serializable;
 
-@Getter
-@Setter
-public class Color{
-    private String color;
-    private String description;
+public class Color {
+    String color;
+    String description;
 
     public Color(String color, String description) {
         this.color = color;
         this.description = description;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getDescription() {
+        return description;
     }
 }
