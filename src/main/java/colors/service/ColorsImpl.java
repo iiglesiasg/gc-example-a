146a8b0f0960aecb4024b7f37720c2dcd3d5ee87
Iñigo Iglesias <ningunpenk@hotmail.com>
2020-10-12 18:37:39 +0200

    stashed

--- src/main/java/colors/service/ColorsImpl.java
+++ src/main/java/colors/service/ColorsImpl.java
@@ -2,13 +2,17 @@ package colors.service;
 
 import colors.model.Color;
 import io.reactivex.Flowable;
+import lombok.extern.slf4j.Slf4j;
+
 
 import javax.inject.Singleton;
 
+@Slf4j
 @Singleton
 public class ColorsImpl {
 
     public Flowable<Color> getColors() {
+        log.info("Saludos Terricolas");
         return Flowable.just(new Color("Green", "Grass is green"),
                 new Color("Blue", "Blue is the ocean"));
     }
